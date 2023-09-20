6c90558cfa2fd4381fa970f94184d502f15aef00
DevExpressExampleBot <fakeEmail>
2023-09-20 17:03:58 +0400

    Source auto update [skip ci]

--- VB/LinqServerMode/Main.vb
+++ VB/LinqServerMode/Main.vb
@@ -1,35 +1,27 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Windows.Forms
-Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.Data.Linq
 Imports DevExpress.XtraEditors
 
 Namespace LinqServerMode
-	Partial Public Class Main
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
 
-			linqInstantFeedbackSource.KeyExpression = "SupplierID"
-			gridControl1.DataSource = linqInstantFeedbackSource
-		End Sub
+    Public Partial Class Main
+        Inherits XtraForm
 
+        Public Sub New()
+            InitializeComponent()
+            linqInstantFeedbackSource.KeyExpression = "SupplierID"
+            gridControl1.DataSource = linqInstantFeedbackSource
+        End Sub
 
-		Private Sub OnGetQueryable(ByVal sender As Object, ByVal e As GetQueryableEventArgs) Handles linqInstantFeedbackSource.GetQueryable
-			Dim dt As New NorthwindClassesDataContext()
-			e.QueryableSource = dt.Suppliers
-			e.Tag = dt
-		End Sub
+        Private Sub OnGetQueryable(ByVal sender As Object, ByVal e As GetQueryableEventArgs)
+            Dim dt As NorthwindClassesDataContext = New NorthwindClassesDataContext()
+            e.QueryableSource = dt.Suppliers
+            e.Tag = dt
+        End Sub
 
-		Private Sub OnDismissQueryable(ByVal sender As Object, ByVal e As GetQueryableEventArgs) Handles linqInstantFeedbackSource.DismissQueryable
-			TryCast(e.Tag, NorthwindClassesDataContext).Dispose()
-		End Sub
-	End Class
+        Private Sub OnDismissQueryable(ByVal sender As Object, ByVal e As GetQueryableEventArgs)
+            TryCast(e.Tag, NorthwindClassesDataContext).Dispose()
+        End Sub
+    End Class
 End Namespace
--- VB/LinqServerMode/Properties/AssemblyInfo.vb
+++ VB/LinqServerMode/Properties/AssemblyInfo.vb
@@ -1,28 +1,23 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("LinqServerMode")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("LinqServerMode")>
-<Assembly: AssemblyCopyright("Copyright ©  2009")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("LinqServerMode")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("LinqServerMode")>
+<Assembly:AssemblyCopyright("Copyright ©  2009")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("a2193168-541b-4f39-b0c7-80775acb9335")>
-
+<Assembly:Guid("a2193168-541b-4f39-b0c7-80775acb9335")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -33,5 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
